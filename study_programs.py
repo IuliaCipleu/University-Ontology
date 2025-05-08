4f5afaaf434cd3a8c8467e2106fdbfae29004e4b
Iulia Cipleu <115873337+IuliaCipleu@users.noreply.github.com>
2025-05-08 12:39:22 +0300

    hasLengthOfYears

--- study_programs.py
+++ study_programs.py
@@ -4,20 +4,16 @@ from PIL import Image
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import RDF, OWL, RDFS
 
-
 os.environ['TESSDATA_PREFIX'] = r'C:\Program Files\Tesseract-OCR\tessdata'
 
 # Set the path to tesseract if it's not in your PATH
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'  # Update this path based on your system
 
 # Open an image file
-img = Image.open('fig\study_programme.jpg')  # Replace 'image.png' with the path to your image
+img = Image.open('fig\\study_programme.jpg')  # Replace 'image.png' with the path to your image
 
 # Use pytesseract to extract text
 text = pytesseract.image_to_string(img)
-
-# Print the extracted text
-print(text)
 
 # Split the text into lines
 lines = text.split("\n")
@@ -54,15 +50,6 @@ for line in lines:
         elif current_degree == "M.Sc.":
             master_programs.append(program_data)
 
-# Print parsed programs
-print("B.Sc. Programs:")
-for program in bachelor_programs:
-    print(program)
-
-print("\nM.Sc. Programs:")
-for program in master_programs:
-    print(program)
-
 # Create a graph for the OWL file
 g = Graph()
 
@@ -75,6 +62,9 @@ StudyProgram = URIRef(base_uri + "StudyProgram")
 # Create subclasses for B.Sc. and M.Sc.
 BSc = URIRef(base_uri + "BSc")
 MSc = URIRef(base_uri + "MSc")
+
+# Create the data property for length of time (years)
+hasLengthOfYears = URIRef(base_uri + "hasLengthOfYears")
 
 # Create individuals for each B.Sc. and M.Sc. program
 for program in bachelor_programs:
@@ -91,6 +81,9 @@ for program in bachelor_programs:
     for lang in program['languages']:
         g.add((program_uri, URIRef(base_uri + "hasLanguage"), Literal(lang)))
 
+    # Add the length of time (4 years for BSc)
+    g.add((program_uri, hasLengthOfYears, Literal(4)))
+
 for program in master_programs:
     # Create a URI for each program
     program_uri = URIRef(base_uri + program['name'].replace(" ", "_") + "_MSc")
@@ -104,6 +97,9 @@ for program in master_programs:
     g.add((program_uri, URIRef(base_uri + "hasLocation"), Literal(program['location'])))
     for lang in program['languages']:
         g.add((program_uri, URIRef(base_uri + "hasLanguage"), Literal(lang)))
+
+    # Add the length of time (2 years for MSc)
+    g.add((program_uri, hasLengthOfYears, Literal(2)))
 
 # Serialize and save the data as an OWL file
 g.serialize("study_programs.owl", format="xml")
